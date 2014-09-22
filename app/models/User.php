ef97c3f5b6a705e4af49d8b84ea252220d2ba0a7
joychao <anzhengchao@gmail.com>
2014-09-22 23:34:36 +0800

    个性User模型fillable属性

--- app/models/User.php
+++ app/models/User.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Eloquent\Model;
 class User extends Model 
 {   
     protected $table = 'user';
-    protected $fillable   = [];
+    protected $fillable = ['*'];
     public $timestamps = true;
 
 }
