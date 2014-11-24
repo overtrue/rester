86a9a3f52a998d2fb0e91ab483f24aa10a0875ef
安正超(Carlos) <anzhengchao@gmail.com>
2014-11-24 22:24:08 +0800

    删除多余的分页方法

--- app/models/Model.php
+++ app/models/Model.php
@@ -4,27 +4,5 @@ use Illuminate\Database\Eloquent\Model as Eloquent;
 
 class Model extends Eloquent
 {
-    /**
-     * 重写paginate方法
-     *
-     * @param integer $integer
-     * @param array   $columns
-     * @param boolean $justItems 是否只返回items
-     *
-     * @return mixed
-     */
-    public static function paginate($perPage, $columns = array('*'), $offset = null)
-    {
-        if (empty($this)) {
-            $model = new static;
-        }
 
-        if (is_null($offset)) {
-            $currentPage = empty($_GET['page']) ? 1 : abs($_GET['page']);
-        }
-
-        $offset = ($currentPage - 1) * $perPage;
-
-        return $model->getQuery()->skip($offset)->select($columns)->take($perPage)->get();
-    }
 }
